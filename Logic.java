41c917bb5bb47f8a77c22edd19e4006032f01cee
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-01-31 19:31:52 +0530

    added program on find even number from array

---
+++ Logic.java
@@ -0,0 +1,13 @@
+class Logic {
+    public static void main(String[] args) {
+        int[] arr= {12,34,5,3,39,79,40,56};
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] % 2==0){
+              System.out.println("even number are:"+ arr[i]);
+            }
+            
+        }
+        
+    }
+}
