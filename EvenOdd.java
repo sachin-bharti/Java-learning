f6a6b717449953e2f5bf8f43e0fa8c0c9c5bf1bd
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-11 12:27:26 +0530

    added program to check even or number

---
+++ EvenOdd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class EvenOdd {
+    public static void checkEvenOdd(int number) {
+        if (number % 2 == 0){
+            System.out.println("even number :" + number);
+        }
+        else {
+            System.out.println("Odd number :" + number);
+        }
+    }
+    public static void main(String[] args){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = sc.nextInt();
+        checkEvenOdd(number);
+    }
+
+  
+}
--- TODO.md
+++
@@ -1,6 +0,0 @@
-# TODO: Refactor EvenOdd.java to use separate method with user input
-
-- [x] Modify main method to take user input for the number using Scanner
-- [x] Create a separate method `checkEvenOdd(int number)` that performs the even/odd check and prints the result
-- [x] Call the new method from main
-- [x] Test the program to ensure it works correctly
