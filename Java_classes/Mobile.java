c44ad2d4427e16eb71688c8b74f25998919b3a58
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-14 23:48:06 +0530

    i dont know why i added this

---
+++ Java_classes/Mobile.java
@@ -0,0 +1,13 @@
+package Java_classes;
+public class Mobile {
+    String MBrand;
+    int price ;
+    void Iphone() {
+        System.out.println("this my first phone :");
+    }
+    public static void main(String[] args) {
+       Mobile m1 = new Mobile(); 
+       m
+    }
+
+}
