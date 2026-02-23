25db1a77a4cb5bd4817e0311950bcba3018c43b3
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-23 19:54:35 +0530

    added program to create a class and object

--- Java_classes/Mobile.java
+++ Java_classes/Mobile.java
@@ -2,12 +2,29 @@ package Java_classes;
 public class Mobile {
     String MBrand;
     int price ;
-    void Iphone() {
+    void firstPhone() {
         System.out.println("this my first phone :");
+        System.out.println("Mobile name :" + MBrand);
+        System.out.println("Price :" + price);
+    }
+     void secondphone() {
+        System.out.println("this my second phone :");
+        System.out.println("Mobile name :" + MBrand);
+        System.out.println("Price :" + price);
     }
     public static void main(String[] args) {
        Mobile m1 = new Mobile(); 
-       m
+       m1.MBrand = " VIVO";
+       m1.price = 15000;
+       m1.firstPhone();
+       Mobile m2 = new Mobile();
+       m2.MBrand = "Iqoo Z7";
+       m2.price = 25000;
+       m2.secondphone();
+
+
+       
+       
     }
 
 }
