63c09fe04032686cbdbd93dfbb9dd56aaaa7e96d
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-26 17:42:09 +0530

    replacing charector from string

---
+++ java_strings_practice_set/NewString_2.java
@@ -0,0 +1,12 @@
+// program to replace space with underscore 
+package java_strings_practice_set;
+
+public class NewString_2 {
+    public static void main(String[] args) {
+      String s = "Hello Sky lord";
+      System.out.println(s);
+      s = s.replace(" ", "_") ;
+      System.out.println(s);
+        
+    }
+}
