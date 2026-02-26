408d6d7c8b7b3c77b66b67a4995fa047ba6942c7
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-26 17:00:26 +0530

    converting string to lower case

---
+++ java_strings_practice_set/NewString.java
@@ -0,0 +1,11 @@
+//Question : Write a java program to convert string to lowercase 
+package java_strings_practice_set;
+
+public class NewString {
+    public static void main(String[] args) {
+        String s = "HELLO SKY LORD";
+        System.out.println(s);
+        System.out.println(s.toLowerCase());
+        
+    }
+}
