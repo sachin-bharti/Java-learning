b392f71d810933128a6a0061f3e8dfb69d6680c1
sachin-bharti <sachinbharti.offcl@gmail.com>
2026-02-27 15:26:17 +0530

    pata nhi kya tha

---
+++ java_strings_practice_set/String_Construction.java
@@ -0,0 +1,15 @@
+package java_strings_practice_set;
+class  MyCar{
+    MyCar(){
+        String name = "BMW M3 GTR";
+        int price = 100000;
+        String color = "White with blue strip";
+    }
+}
+public class String_Construction {
+    public static void main(String[] args) {
+        MyCar car1= new MyCar();
+
+        
+    }
+}
